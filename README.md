6c164091c4bf7c7d9633548e1d9c163db6204c1b
Apoorav <medal.apoorav@gmail.com>
2023-04-04 16:00:26 +0530

    readme update

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # NN-from-scratch
 My attempt at the implementation of a neural network using numpy.
+
+## Try
+
+```
+import numpy as np            
+X = [35, 83, 17, 14, 90, 90, 45, 52, 60, 83, 73, 97, 70, 13, 73, 69, 6, 6, 6, 8]
+y = np.array([1, 0, 0, 0])
+
+nn = NN(X, y, 6)
+
+print("Before Train:", nn.getArgMax())
+nn.train()
+print("After Train:", nn.getArgMax())
+```
--- main.py
+++ main.py
@@ -206,13 +206,3 @@ class NN:
             self.cost_history.append(error)
             self.backpass()
             self.forward()
-
-import numpy as np            
-X = [35, 83, 17, 14, 90, 90, 45, 52, 60, 83, 73, 97, 70, 13, 73, 69, 6, 6, 6, 8]
-y = np.array([1, 0, 0, 0])
-
-nn = NN(X, y, 6)
-
-print("Before Train:", nn.getArgMax())
-nn.train()
-print("After Train:", nn.getArgMax())
